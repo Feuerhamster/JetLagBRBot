914a3817f22757a6c669e0599a4a651fd4edfb73
Lena Emme <38376566+feuerhamster@users.noreply.github.com>
2024-12-24 15:22:02 +0100

    add await to base power up timer finished method

--- Game/Modes/BattleRoyale/Utils/BasePowerUp.cs
+++ Game/Modes/BattleRoyale/Utils/BasePowerUp.cs
@@ -61,7 +61,7 @@ public abstract class BasePowerUp(BattleRoyaleGamemode gamemode, Guid ownerId)
     
     protected virtual async Task OnTimerFinished()
     {
-        this.Expire();
+        await this.Expire();
     }
 
     private void Tick(object? sender, EventArgs e)
